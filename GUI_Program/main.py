39fbc495ee20bb94f2a494896ab57cd9d13d6049
Oscar Ryley <oscar.ryley@gmail.com>
2023-11-23 15:52:15 +0000

    Ouptuts working using prints and sg.Output workaround
    No text highlight colour or real-type printing as of yet, but this appears difficult to achieve

--- GUI_Program/main.py
+++ GUI_Program/main.py
@@ -3,7 +3,6 @@
 import random
 import os
 import math
-import textwrap
 import PySimpleGUI as sg
 
 os.system('cls')
@@ -14,18 +13,14 @@ raw_words = file.readlines()
 for word in raw_words:
     accepted_words.append(word.strip())
 
-
 def print_colour(word_list, aim):
-    word = []
     greend = []
     score = []
-    if len(word_list) < len(aim):
-        return [0,0,0,0,0], ["T", "R", "I", "E", "D", "\n"]
     for i in range(0, len(aim)):
         x = 0
         y = 0
         if word_list[i] == aim[i]:
-            word.append(str(word_list[i]))
+            print(str(word_list[i]), end="")
             greend.append(word_list[i])
             score.append(2)
         else:
@@ -35,14 +30,14 @@ def print_colour(word_list, aim):
                         if word_list[i] == k:
                             y = 1
                     if y == 0:
-                        word.append(str(word_list[i]))
+                        print(str(word_list[i]), end="")
                         score.append(1)
                         x = 1
             if x == 0:
-                word.append(str(word_list[i]))
+                print(str(word_list[i]), end="")
                 score.append(0)
-    word.append("\n")
-    return score, word
+    print("\n")
+    return score
 
 def pure_rand(chimps, aim, printable):
     tries = 0
@@ -56,23 +51,20 @@ def pure_rand(chimps, aim, printable):
             if len(list(appendable)) > 1:
                 appendable = ""
             chimps.append(appendable)
-        output = print_colour(chimps, aim)
+        print_colour(chimps, aim)
         tries += 1
-    return output, tries
-
+    return tries
 
 def rand_words(chimps, aim):
-    output = []
     tries = 0
     while chimps != aim:
         chimps = list(random.choice(accepted_words).upper())
-        output.append(print_colour(chimps, aim))
+        print_colour(chimps, aim)
         tries += 1
-    return output, tries
+    return tries
 
 
 def minor_intelligence(chimps, aim, printable):
-    output = []
     tries = 1
     last = list(random.choice(accepted_words).upper())
     score = print_colour(last, aim)
@@ -100,35 +92,27 @@ def minor_intelligence(chimps, aim, printable):
             index += 1
         last = chimps
         score = print_colour(chimps, aim)
-        output.append(score)
         tries += 1
-    return output, tries
-
+    return tries
 
 def mode(choice, aim, printable):
-    printable = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-    "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    ]
-    output = []
     chimps = []
     if choice == "er":
-        output, tries = pure_rand(chimps, aim, printable)
+        tries = pure_rand(chimps, aim, printable)
     elif choice == "rc":
-        output, tries = rand_words(chimps, aim)
+        tries = rand_words(chimps, aim)
     elif choice == "mr":
-        output, tries = minor_intelligence(chimps, aim, printable)
-    return output, tries
+        tries = minor_intelligence(chimps, aim, printable)
+    return tries
 
 def main():
     sg.theme("SystemDefaultForReal")
     width = 420
     height = 700
-    output = [[sg.Text(key="output")]]
     SearchScreenLayout = [
     [sg.Button("Entirely random", enable_events = True), sg.Button("Random correct words", enable_events = True), sg.Button("Minor Intelligence?", key = "mr", enable_events = True)],
     [sg.Text("Enter the Wordle:"), sg.InputText(size = (25,1), key = "iwordle") , sg.Button("Enter", enable_events = True)],
-    [sg.Column(output, scrollable=True,  vertical_scroll_only=True, size = (420, 700), key = "box")]
+    [sg.Output(size = (420, 700))]
     ]
     window = sg.Window("Chimps on Wordle", SearchScreenLayout, size = (width,height), resizable = True)
     printable = [
@@ -137,7 +121,6 @@ def main():
     ]
     cmode = "rc"
     while True:
-        chimps = []
         tries = 0
         event, values = window.read()
 
@@ -148,15 +131,12 @@ def main():
             cmode = "rc"
 
         if event == "Minor Intelligence?":
-            cmode == "mr"
+            cmode = "mr"
 
         if event == "Enter":
             total = 0
             aim = []
-            try:
-                phrase = (values["iwordle"]).upper()
-            except:
-                pass
+            phrase = (values["iwordle"]).upper()
             aim = list(phrase)
             for i in aim:
                 for j in printable:
@@ -165,21 +145,11 @@ def main():
             for i in accepted_words:
                 isword = phrase.lower()
                 if str(isword) == str(i) and total == 5:
-                    output, tries = mode(cmode, aim, printable)
-                    for i in output:
-                        for j in i:
-                            if str(j[0]).isdigit() == False:
-                                word = ""
-                                for k in j:
-                                    word = word + k
-                                newtext = textwrap.fill(word, 40)
-                                window["output"].update(str(window["output"].get() + newtext + "\n"))
-                                window.refresh()
-
-                    window["output"].update(str(window["output"].get() +"DONE in {} tries".format(tries)))
-                    window["output"].update(str(window["output"].get() +"That would take", math.ceil(tries / 6), "days"))
-                    window["output"].update(str(window["output"].get() +"Which is", round((tries / 6) / 365, 2), "years"))
-            
+                    tries = mode(cmode, aim, printable)
+                    print("DONE in {} tries".format(tries))
+                    print("That would take", math.ceil(tries / 6), "days")
+                    print("Which is", round((tries / 6) / 365, 2), "years")
+                    
         if event == sg.WIN_CLOSED:
             window.close()
             break

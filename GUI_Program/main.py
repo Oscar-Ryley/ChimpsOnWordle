e8b04eeeb801ec1b00dd6f4c4d8c7fb91fcaf1cf
Oscar Ryley <oscar.ryley@gmail.com>
2023-11-23 13:53:54 +0000

    Further Developed GUI solution
    Outputs output
    Current problem: box does not allow scrolling down with/ to new text output once it leaves range of box

--- GUI_Program/main.py
+++ GUI_Program/main.py
@@ -3,6 +3,7 @@
 import random
 import os
 import math
+import textwrap
 import PySimpleGUI as sg
 
 os.system('cls')
@@ -18,11 +19,13 @@ def print_colour(word_list, aim):
     word = []
     greend = []
     score = []
+    if len(word_list) < len(aim):
+        return [0,0,0,0,0], ["T", "R", "I", "E", "D", "\n"]
     for i in range(0, len(aim)):
         x = 0
         y = 0
         if word_list[i] == aim[i]:
-            word.append(str("\033[1;37;42m" + str(word_list[i])))
+            word.append(str(word_list[i]))
             greend.append(word_list[i])
             score.append(2)
         else:
@@ -32,13 +35,13 @@ def print_colour(word_list, aim):
                         if word_list[i] == k:
                             y = 1
                     if y == 0:
-                        word.append(str("\033[1;37;43m" + str(word_list[i])))
+                        word.append(str(word_list[i]))
                         score.append(1)
                         x = 1
             if x == 0:
-                word.append(str("\033[1;37;40m" + str(word_list[i])))
+                word.append(str(word_list[i]))
                 score.append(0)
-    word.append("\033[1;37;40m\n")
+    word.append("\n")
     return score, word
 
 def pure_rand(chimps, aim, printable):
@@ -107,39 +110,46 @@ def mode(choice, aim, printable):
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
     "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     ]
+    output = []
     chimps = []
-    while True:
-        if choice == "a":
-            output, tries = pure_rand(chimps, aim, printable)
-            break
-        elif choice == "b":
-            output, tries = rand_words(chimps, aim)
-            break
-        elif choice == "c":
-            output, tries = minor_intelligence(chimps, aim, printable)
-            break
+    if choice == "er":
+        output, tries = pure_rand(chimps, aim, printable)
+    elif choice == "rc":
+        output, tries = rand_words(chimps, aim)
+    elif choice == "mr":
+        output, tries = minor_intelligence(chimps, aim, printable)
     return output, tries
 
 def main():
     sg.theme("SystemDefaultForReal")
-    width = 1000
+    width = 420
     height = 700
-    output = [[sg.Text("Output", key="output")]]
+    output = [[sg.Text(key="output")]]
     SearchScreenLayout = [
-    [sg.Button("Entirely random", key= "er", enable_events = True), sg.Button("Random correct words", key = "rc", enable_events = True), sg.Button("Minor Intelligence?", key = "mr", enable_events = True)],
+    [sg.Button("Entirely random", enable_events = True), sg.Button("Random correct words", enable_events = True), sg.Button("Minor Intelligence?", key = "mr", enable_events = True)],
     [sg.Text("Enter the Wordle:"), sg.InputText(size = (25,1), key = "iwordle") , sg.Button("Enter", enable_events = True)],
-    [sg.Column(output, scrollable=True,  vertical_scroll_only=True, size = (1920, 1080), expand_x = True, expand_y = True)]
+    [sg.Column(output, scrollable=True,  vertical_scroll_only=True, size = (420, 700), key = "box")]
     ]
-    window = sg.Window("The Fountain of Knowledge", SearchScreenLayout, size = (width,height), resizable = True)
+    window = sg.Window("Chimps on Wordle", SearchScreenLayout, size = (width,height), resizable = True)
     printable = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
     "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     ]
-    choice = "c"
+    cmode = "rc"
     while True:
         chimps = []
         tries = 0
         event, values = window.read()
+
+        if event == "Entirely random":
+            cmode = "er"
+
+        if event == "Random correct words":
+            cmode = "rc"
+
+        if event == "Minor Intelligence?":
+            cmode == "mr"
+
         if event == "Enter":
             total = 0
             aim = []
@@ -155,23 +165,24 @@ def main():
             for i in accepted_words:
                 isword = phrase.lower()
                 if str(isword) == str(i) and total == 5:
-                    output, tries = mode(choice, aim, printable)
-                    output += ("DONE in {} tries".format(tries))
-                    output += ("That would take", math.ceil(tries / 6), "days")
-                    output += ("Which is", round((tries / 6) / 365, 2), "years")
+                    output, tries = mode(cmode, aim, printable)
+                    for i in output:
+                        for j in i:
+                            if str(j[0]).isdigit() == False:
+                                word = ""
+                                for k in j:
+                                    word = word + k
+                                newtext = textwrap.fill(word, 40)
+                                window["output"].update(str(window["output"].get() + newtext + "\n"))
+                                window.refresh()
+
+                    window["output"].update(str(window["output"].get() +"DONE in {} tries".format(tries)))
+                    window["output"].update(str(window["output"].get() +"That would take", math.ceil(tries / 6), "days"))
+                    window["output"].update(str(window["output"].get() +"Which is", round((tries / 6) / 365, 2), "years"))
             
         if event == sg.WIN_CLOSED:
             window.close()
             break
-        
-        if event == "er":
-            choice = "a"
-
-        if event == "rc":
-            choice = "b"
-
-        if event == "mr":
-            choice == "c"
 
 if __name__ == "__main__":
     main()
